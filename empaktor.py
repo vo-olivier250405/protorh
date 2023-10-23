0273f54958b5ff024e1cf262c06550c13d50b6ac
vo_o <vo_o@etna-alternance.net>
2023-10-23 17:11:40 +0200

    [ADD] => empaktor.py => Ajout d'un fichier qui ouvre un autre fichier, l'encode, et l'archive

---
+++ empaktor.py
@@ -0,0 +1,64 @@
+"""
+Module qui va compresser les fichiers à l'aide d'une des 3 méthodes.
+"""
+
+from sys import argv
+import tarfile
+from base64 import b64encode
+from cmp_rle.rle import encode_rle, decode_rle
+
+
+def open_file(name: str) -> str:
+    """
+    Ouvre un fichier et lit les infos dedans. Puis le ferme.
+    """
+    file = open(name, "r")
+    data = file.read()
+    file.close()
+    return data
+
+
+def rewrite_file(txt: str, name: str) -> None:
+    """
+    Réécrit dans un fichier
+    """
+    file = open(name, "a")
+    file.truncate(0)
+    file.write(txt)
+    # file.read()
+    file.close()
+
+
+def filter_txt(name: str) -> str:
+    """
+    Retire tout les délimiteurs
+    """
+    name = name.replace("/", "").replace("|", "")
+    return name
+
+
+def compress(name: str, file_add: str) -> None:
+    """
+    Fonction qui prend en paramètre le nom du fichier à compresser et
+    en crée une archive
+    """
+    temp = tarfile.open(name, "w:gz")
+    temp.add(file_add)
+    temp.close()
+    return None
+
+
+data = open_file(argv[1])
+coded = encode_rle(data=data)
+decoded = decode_rle(encoded_data=coded)
+
+rewrite_file(filter_txt(coded), argv[1])
+
+# Compresse
+compress("test.tar.gz", "cmp_rle/test.txt")
+
+# Décompresse
+# folder = tarfile.open("test.tar.gz", "r")
+#
+# extr = folder.extractfile("cmp_rle/test.txt")
+# rewrite_file(decoded, "cmp_rle/test.txt")
