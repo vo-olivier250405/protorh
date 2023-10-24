cc32a231396269ff543da675058f266ceaa7ee61
vo_o <vo_o@etna-alternance.net>
2023-10-24 10:47:50 +0200

    [ADD] => Ajout d'une fonction qui récupère les fichiers à compresser, compresse tout les fichiers placés en arguments

--- empaktor.py
+++ empaktor.py
@@ -37,13 +37,21 @@ def filter_txt(name: str) -> str:
     return name
 
 
-def compress(name: str, file_add: str) -> None:
+def compress(name: str, file_add: list) -> None:
     """
     Fonction qui prend en paramètre le nom du fichier à compresser et
     en crée une archive
     """
     temp = tarfile.open(name, "w:gz")
-    temp.add(file_add)
+    for files in file_add:
+        print(f"\nCompression de: {files}...")
+        coded = encode_rle(data=open_file(files))
+        decoded = decode_rle(encoded_data=coded)
+        rewrite_file(filter_txt(coded), files)
+        temp.add(files)
+        rewrite_file(decoded, files)
+        # temp.add(rewrite_file(filter_txt(coded), files))
+        print(f"Compression de: {files} dans {name}: [\x1b[32mOK\x1b[0m]")
     temp.close()
     return None
 
@@ -52,20 +60,36 @@ def uncompress(name: str) -> None:
     """
     Fonction qui décomppresse l'archive
     """
+    print(f"\nDécompression de {name}...")
     file = tarfile.open(name)
     file.extractall(f"./{name[:8]}")
-    print("Décompréssé(s): ", file.getnames())
+    print(f"Décompression de {name}: [\x1b[32mOK\x1b[0m]")
+    print(f"Éléments décompréssé(s): {file.getnames()}")
     file.close()
 
 
-data = open_file(argv[1])
-coded = encode_rle(data=data)
-decoded = decode_rle(encoded_data=coded)
+def get_files(argvs: list) -> list:
+    """
+    Récupère les fichiers à compresser et les renvoie
+    """
+    file_add: list = []
+    for i in range(3, len(argvs)):
+        file_add.append(argvs[i])
+    return file_add
 
-rewrite_file(filter_txt(coded), argv[1])
-
-# Compresse
-compress("test.tar.gz", "cmp_rle/test.txt")
-# Décompresse
-uncompress("test.tar.gz")
-rewrite_file(decoded, "test.tar/cmp_rle/test.txt")
+if argv[2] == "--compression":
+    compress(argv[1], get_files(argv))
+else:
+    print(f"\x1b[31mErreur: \x1b[0mArgument {argv[2]} non valide.")
+# nom_archive.tar.gz --compression nom_algorithme fichier1 fichier2 fichier3 ...
+# data = open_file(argv[1])
+# coded = encode_rle(data=data)
+# decoded = decode_rle(encoded_data=coded)
+#
+# rewrite_file(filter_txt(coded), argv[1])
+#
+# # Compresse
+# compress("test.tar.gz", "cmp_rle/test.txt")
+# # Décompresse
+# uncompress("test.tar.gz")
+# rewrite_file(decoded, "test.tar/cmp_rle/test.txt")
