3b11ffaa1057651e904d040cd5d3dd0372ee42e5
vo_o <vo_o@etna-alternance.net>
2023-10-25 10:06:34 +0200

    [ADD] => empaktor.py =>  Ajout des tests

--- empaktor/empaktor.py
+++ empaktor/empaktor.py
@@ -52,13 +52,13 @@ def filter_txt(name: str) -> str:
     return name
 
 
-def compress(name: str, file_add: list) -> None:
+def compress(name: str, folder: list) -> None:
     """
     Fonction qui prend en paramètre le nom du fichier à compresser et
     en crée une archive
     """
     temp = tarfile.open(name, "w:gz")
-    for files in file_add:
+    for files in folder:
         print(f"\nCompression de: {files}...")
         coded = encode_rle(data=open_file(files))
         decoded = decode_rle(encoded_data=coded)
@@ -89,36 +89,74 @@ def get_files(argvs: list) -> list:
     Récupère les fichiers à compresser et les renvoie
     """
     file_add: list = []
-    for i in range(3, len(argvs)):
+    for i in range(4, len(argvs)):
         file_add.append(argvs[i])
     return file_add
 
 
-def get_method(method_name: str) -> function:
+def recode_file(path: str):
     """
-    Renvoi le nom de la méthode de compression
+    Recode le fichier après la décompression
     """
-    return {"rle": [encode_rle, decode_rle],
-            "huffman": [huffman],
-            "burrows_wheeler": [burrows_wheeler]}[method_name]
+    for file in listdir(path):
+        data = open_file(f"{path}/{file}")
+        decoded = decode_rle(encoded_data=data)
+        rewrite_file(decoded, f"{path}/{file}")
 
 
-def main(method: str) -> None:
+def check_arg_compression() -> bool:
+    """
+    Teste toutes les erreurs possibles
+    """
+    if argv[3] not in ["rle", "huffman", "burrows_wheeler"]:
+        print(f"\n\x1b[31mErreur: \x1b[0mArgument {argv[3]} non valide.")
+        print(f"{argv[3]} n'est pas un algorithme proposé")
+        return False
+    elif get_files(argv) == []:
+        print(f"\n\x1b[31mErreur: \x1b[0mLes fichiers n'existent pas.")
+        return False
+    return True
+
+
+def check_arg_extract() -> bool:
+    """
+    Vérifie les erreurs pour la décompression
+    """
+    if len(argv) != 2:
+        print(f"\n\x1b[31mErreur: \x1b[0mCe n'est pas la bonne commande\n")
+        return False
+    return True
+
+
+def main() -> bool:
     """
     Lance le main pour permettre de décomprésser ou compresser
     """
-    print(method)
-    if argv[2] == "--compression":
-        compress(argv[1], get_files(argv))
-    elif argv[2] == "--décompression":
-        path = uncompress(argv[1])
-        for file in listdir(path):
-            data = open_file(f"{path}/{file}")
-            decoded = decode_rle(encoded_data=data)
-            rewrite_file(decoded, f"{path}/{file}")
+    # Vérifie si on veut décompresser
+    if argv == ["empaktor.py"]:
+        print(f"\n\x1b[31mErreur: \x1b[0mVous n'avez rien saisi\n")
+        return False
+    elif len(argv) <= 3:
+        print(f"\n\x1b[31mErreur: \x1b[0mCe n'est pas la bonne commande\n")
+        return False
+    elif argv[2] == "--compression":
+        valid: bool = check_arg_compression()
+        if valid:
+            compress(argv[1], get_files(argv))
+        return valid
+    # Vérifie si l'on veut décompresser
+    elif argv[1] == "--extract":
+        valid: bool = check_arg_extract()
+        if valid:
+            path = uncompress(argv[2])
+            recode_file(path)
+        return valid
+    # Vérifie les erreurs
     else:
-        print(f"\x1b[31mErreur: \x1b[0mArgument {argv[2]} non valide.")
-    return None
+        print(f"\n\x1b[31mErreur: \x1b[0mArgument {argv[2]} non valide.")
+        print("Il faut utiliser --compression ou --extract.")
+        return False
 
 
-print(get_method(argv[3]))
+if not main():
+    print("Tapez: \x1b[32mpython3 empaktor.py --help\x1b[0m pour de l'aide.")
