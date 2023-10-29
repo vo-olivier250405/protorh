693821bf2b991ce8a9577271c973570347994f28
vo_o <vo_o@etna-alternance.net>
2023-10-29 17:06:40 +0100

    [ADD] => empaktor.py => pep8

--- empaktor/empaktor.py
+++ empaktor/empaktor.py
@@ -15,9 +15,9 @@ def open_file(name: str) -> str:
     """
     Ouvre un fichier et lit les infos dedans. Puis le ferme.
     """
-    file = open(name, "r")
-    data = file.read()
-    file.close()
+    with open(name, "r", encoding="utf-8") as file:
+        data = file.read()
+        file.close()
     return data
 
 
@@ -25,11 +25,11 @@ def rewrite_file(txt: str, name: str) -> None:
     """
     Réécrit dans un fichier
     """
-    file = open(name, "a")
-    file.truncate(0)
-    file.write(txt)
-    # file.read()
-    file.close()
+    with open(name, "a", encoding="utf-8") as file:
+        file.truncate(0)
+        file.write(txt)
+        # file.read()
+        file.close()
 
 
 def filter_txt(name: str) -> str:
@@ -45,18 +45,17 @@ def compress(name: str, folder: list) -> None:
     Fonction qui prend en paramètre le nom du fichier à compresser et
     en crée une archive
     """
-    temp = tarfile.open(name, "w:gz")
-    for files in folder:
-        print(f"\nCompression de: {files}...")
-        coded = encode_rle(data=open_file(files))
-        decoded = decode_rle(encoded_data=coded)
-        rewrite_file(coded, files)
-        temp.add(files)
-        rewrite_file(decoded, files)
-        # temp.add(rewrite_file(filter_txt(coded), files))
-        print(f"Compression de: {files} dans {name}: [\x1b[32mOK\x1b[0m]")
-    temp.close()
-    return None
+    with tarfile.open(name, "w:gz") as temp:
+        for files in folder:
+            print(f"\nCompression de: {files}...")
+            coded = encode_rle(data=open_file(files))
+            decoded = decode_rle(encoded_data=coded)
+            rewrite_file(coded, files)
+            temp.add(files)
+            rewrite_file(decoded, files)
+            # temp.add(rewrite_file(filter_txt(coded), files))
+            print(f"Compression de: {files} dans {name}: [\x1b[32mOK\x1b[0m]")
+        temp.close()
 
 
 def uncompress(name: str) -> None:
@@ -64,11 +63,11 @@ def uncompress(name: str) -> None:
     Fonction qui décomppresse l'archive
     """
     print(f"\nDécompression de {name}...")
-    file = tarfile.open(name)
-    file.extractall(f"./{name[:-7]}")
-    print(f"Décompression de {name}: [\x1b[32mOK\x1b[0m]")
-    print(f"Éléments décompréssé(s): {file.getnames()}")
-    file.close()
+    with tarfile.open(name) as file:
+        file.extractall(f"./{name[:-7]}")
+        print(f"Décompression de {name}: [\x1b[32mOK\x1b[0m]")
+        print(f"Éléments décompréssé(s): {file.getnames()}")
+        file.close()
     return f"./{name[:-7]}"
 
 
@@ -117,7 +116,7 @@ def check_arg_compression() -> bool:
         return False
     # Teste si l'utilisateur a saisi le bon nombre d'argument
     if len(argv) == 3:
-        print(f"\n\x1b[31mErreur: \x1b[0mCommande non valide.")
+        print("\n\x1b[31mErreur: \x1b[0mCommande non valide.")
         return False
     # Test si la méthode utilisée est valide
     if argv[3] not in ["rle", "huffman", "burrows_wheeler"]:
@@ -125,8 +124,8 @@ def check_arg_compression() -> bool:
         print(f"{argv[3]} n'est pas un algorithme proposé.")
         return False
     # Vérifie si il y a des fihciers à compresser
-    elif not get_files(argv):
-        print(f"\n\x1b[31mErreur: \x1b[0mLes fichiers n'existent pas.")
+    if not get_files(argv):
+        print("\n\x1b[31mErreur: \x1b[0mLes fichiers n'existent pas.")
         return False
     return True
 
@@ -146,7 +145,7 @@ def check_arg_extract() -> bool:
         return False
     # Vérifie le nombre correct d'argument
     if len(argv) != 4:
-        print(f"\n\x1b[31mErreur: \x1b[0mCe n'est pas la bonne commande.\n")
+        print("\n\x1b[31mErreur: \x1b[0mCe n'est pas la bonne commande.\n")
         return False
     return True
 
@@ -156,29 +155,28 @@ def method_manager() -> bool:
     Lance le main pour permettre de décomprésser ou compresser
     """
     if argv == ["empaktor.py"]:
-        print(f"\n\x1b[31mErreur: \x1b[0mVous n'avez rien saisi.\n")
+        print("\n\x1b[31mErreur: \x1b[0mVous n'avez rien saisi.\n")
         return False
-    elif len(argv) < 4:
-        print(f"\n\x1b[31mErreur: \x1b[0mCe n'est pas la bonne commande.\n")
+    if len(argv) < 4:
+        print("\n\x1b[31mErreur: \x1b[0mCe n'est pas la bonne commande.\n")
         return False
     # Vérifie si on veut compresser
-    elif argv[2] == "--compression":
+    if argv[2] == "--compression":
         valid: bool = check_arg_compression()
         if valid:
             compress(argv[1], get_files(argv))
         return valid
     # Vérifie si l'on veut décompresser
-    elif argv[1] == "--extract":
+    if argv[1] == "--extract":
         valid: bool = check_arg_extract()
         if valid:
             path = uncompress(argv[3])
             recode_file(path)
         return valid
     # Vérifie les erreurs
-    else:
-        print(f"\n\x1b[31mErreur: \x1b[0mArgument {argv[2]} non valide.")
-        print("Il faut utiliser --compression ou --extract.")
-        return False
+    print(f"\n\x1b[31mErreur: \x1b[0mArgument {argv[2]} non valide.")
+    print("Il faut utiliser --compression ou --extract.")
+    return False
 
 
 def main() -> None:
